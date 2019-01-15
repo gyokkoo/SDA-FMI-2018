9f2fffb51649a9e9a260f282494667bb8e27f4ff
Nezhdetov <gyokan.nezhdetov@gmail.com>
2019-01-15 22:16:00 +0200

    :neckbeard:

---
+++ Homework/Practice_11/1_ShortestPaths.cpp
@@ -0,0 +1,159 @@
+/*
+По даден неорентиран и теглови граф и начален връв s, намерете най-късия път от s до всеки от върховете в графа.
+Всички ребра в графа имат неотрицателно тегло.
+
+Input Format
+
+На първия ред е зададено числото t - броя тестове. Всеки тест се състои от:
+
+На първия ред са дадени числата n и m - броя на върховете и ребрата в графа.
+Всеки от следващите m реда съдържа числата u v w, съответващи на неорентирано ребро (u, v) с тегло w.
+На последния ред е зададено числото s - началния връх.
+Върховете в графа са номерирани с числата от 1 до n.
+
+Constraints
+
+1 <= t <= 10
+
+1 <= n, m, <= 2 * 10^5
+
+0 <= w <= 10^9
+
+Output Format
+
+За всеки тест на нов ред изведете dist[0] dist[1] ... dist[s-1] dist[s+1] ... dist[n], където dist[i] е най-късия път
+от s до i. Ако такъв път не съществува(тоест i не е достижим от s), то dist[i] = -1
+
+Sample Input 0
+
+1
+4 4
+1 2 24
+1 4 20
+3 1 3
+4 3 12
+1
+Sample Output 0
+
+24 3 15
+
+ */
+
+#include <iostream>
+#include <vector>
+#include <limits>
+
+const long long LONG_MAX_VALUE = std::numeric_limits<long long>::max();
+
+typedef std::pair<int, long long> iPair;
+
+class Graph {
+public:
+    int V;
+    int E;
+    std::vector<iPair>* adj;
+
+    Graph(int V, int E) {
+        this->V = V;
+        this->E = E;
+        this->adj = new std::vector<iPair>[V + 1];
+    }
+
+    ~Graph() {
+        delete[] this->adj;
+    }
+
+    void addWeightedEdge(int u, int v, long long weight) {
+        this->adj[u].emplace_back(std::make_pair(v, weight));
+        this->adj[v].emplace_back(std::make_pair(u, weight));
+    }
+
+    long long* getDijkstraShortestPathsFrom(int startVertex) {
+        long long* distances = new long long[this->V + 1];
+        bool* isVisited = new bool[this->V + 1];
+
+        for (int i = 0; i <= this->V; i++) {
+            distances[i] = LONG_MAX_VALUE;
+            isVisited[i] = false;
+        }
+
+        distances[startVertex] = 0;
+
+        for (long long i = 1; i <= this->V; i++) {
+            long long currentVertex = getMinDistanceVertexIndex(distances, isVisited);
+
+            isVisited[currentVertex] = true;
+            std::vector<iPair> current =  adj[currentVertex];
+
+            for (auto pair : current) {
+                long long weight = pair.second;
+                long long nextVertex = pair.first;
+                if (!isVisited[nextVertex] &&
+                    distances[currentVertex] != LONG_MAX_VALUE &&
+                    distances[currentVertex] + weight < distances[nextVertex]) {
+                    distances[nextVertex] = distances[currentVertex] + weight;
+                }
+            }
+        }
+
+        return distances;
+    }
+
+private:
+    long long getMinDistanceVertexIndex(const long long* distances, const bool* isVisited) {
+        long long min = LONG_MAX_VALUE;
+        int minVertex = 0;
+
+        for (int v = 1; v <= this->V; v++) {
+            if (!isVisited[v] && distances[v] <= min) {
+                min = distances[v];
+                minVertex = v;
+            }
+        }
+
+        return minVertex;
+    }
+};
+
+int main() {
+    std::ios::sync_with_stdio(false);
+
+    int t = 0;
+    std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n = 0; // Vertices
+        int m = 0; // Edges
+        std::cin >> n >> m;
+        Graph graph(n, m);
+
+        for (int j = 0; j < m; j++) {
+            int u = 0;
+            int v = 0;
+            long long w = 0;
+            std::cin >> u >> v >> w;
+            graph.addWeightedEdge(u, v, w);
+        }
+
+        int startVertex = 0;
+        std::cin >> startVertex;
+
+        long long* distances = graph.getDijkstraShortestPathsFrom(startVertex);
+
+        for (long long vertex = 1; vertex <= n; vertex++) {
+            if (vertex == startVertex) {
+                continue;
+            }
+
+            if (distances[vertex] == LONG_MAX_VALUE) {
+                std::cout << -1 << " ";
+            } else {
+                std::cout << distances[vertex] << " ";
+            }
+        }
+
+        delete[] distances;
+        std::cout << "\n";
+    }
+
+    return 0;
+}
---
+++ Homework/Practice_11/2_LongLong.cpp
@@ -0,0 +1,120 @@
+/*
+Даден е ориентиран и теглови граф с тегла цели числа. Да се намери дължината на най-дългия път от връх s до връх t. 
+Смята се, че в графа няма цикли с положителна цена. 
+Върховете на графа са номерирани с числата от 1 до n.
+
+Input Format
+На първия ред са зададени числата n, m, s, t
+n - брой върхове в графа
+m - брой ребра в графа
+s - начален връх
+t - краен връх
+
+Следват m реда всеки обозначаващ ребро в графа и съдържащ три числа: v1 v2 w
+v1 - начален връх на реброто
+v2 - краен връх на реброто
+w - тегло на реброто
+
+Constraints
+1 <= n <= 1000
+1 <= m <= 10000
+-10^9 <= w <= 10^9
+
+Output Format
+На единствен ред изведете дължината на най-дългия път от s до t. 
+Ако такъв път не съществува(тоест t не е достижим от s), то изведете -1.
+
+Sample Input 0
+4 5 1 2
+1 2 4
+1 3 1
+3 2 1
+3 4 2
+4 2 2
+
+Sample Output 0
+5
+*/
+
+#include <iostream>
+#include <vector>
+#include <limits>
+
+const long long LONG_MAX_VALUE = std::numeric_limits<long long>::max();
+
+struct Edge {
+public:
+    int source;
+    int destination;
+    long long weight;
+
+    Edge(int source, int destination, long long weight) {
+        this->source = source;
+        this->destination = destination;
+        this->weight = weight;
+    }
+};
+
+class Graph {
+public:
+    int V;
+    int E;
+    std::vector<Edge> edges;
+
+    Graph(int V, int E) {
+        this->V = V;
+        this->E = E;
+    }
+
+    void addWeightedEdge(int u, int v, long long weight) {
+        Edge edge(u, v, -weight);
+        edges.push_back(edge);
+    }
+
+    long long getShortestPath(int startVertex, int endVertex) {
+        long long* distances = new long long[this->V + 1];
+        for (int i = 0; i <= this->V; i++) {
+            distances[i] = LONG_MAX_VALUE;
+        }
+
+        distances[startVertex] = 0;
+
+        for (int i = 0; i < this->V - 1; i++) {
+            for (int j = 0; j < this->E; j++) {
+               Edge currentEdge = this->edges[j];
+               int u = this->edges[j].source;
+               int v = this->edges[j].destination;
+               long long weight = this->edges[j].weight;
+               if (distances[u] != LONG_MAX_VALUE && distances[u] + weight < distances[v]) {
+                   distances[v] = distances[u] + weight;
+               }
+            }
+        }
+
+        return distances[endVertex] == LONG_MAX_VALUE ? 1 : distances[endVertex];
+    }
+};
+
+int main() {
+
+    int n = 0; // Vertices
+    int m = 0; // Edges
+    int s = 0; // Start vertex
+    int t = 0; // End vertex
+
+    std::cin >> n >> m >> s >> t;
+
+    Graph graph(n,m);
+    for (int i = 0; i < m; i++) {
+        int v1 = 0;
+        int v2 = 0;
+        long long weight = 0;
+        std::cin >> v1 >> v2 >> weight;
+
+        graph.addWeightedEdge(v1, v2, weight);
+    }
+
+    std::cout << -graph.getShortestPath(s, t) << "\n";
+
+    return 0;
+}
